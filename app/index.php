783d0d8caf7da67184dad3a64072a4847282c6a2
Lautaro LL <62777637+lautaro-ll@users.noreply.github.com>
2021-03-23 19:35:44 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,1 +1,20 @@
-Hola Mundo
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once '../vendor/autoload.php';
+
+
+
+$app = new \Slim\App([]);
+
+
+
+$app->get('[/]', function (Request $request, Response $response) {    
+    $response->getBody()->write("GET => Bienvenido!!! ,a SlimFramework");
+    return $response;
+
+});
+
+
+$app->run();
