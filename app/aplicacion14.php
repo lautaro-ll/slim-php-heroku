db08ef8d486e59002586aac6978ecc7451e324ff
Lautaro LL <62777637+lautaro-ll@users.noreply.github.com>
2021-03-23 20:49:01 -0300

    update

--- app/aplicacion14.php
+++ app/aplicacion14.php
@@ -2,10 +2,13 @@
 /******************************************************************************
 
 Lemos Lautaro Lucas
-
+Aplicación No 14 (Par e impar)
+Crear una función llamada esPar que reciba un valor entero como parámetro y devuelva TRUE
+si este número es par ó FALSE si es impar.
+Reutilizando el código anterior, crear la función esImpar.
 
 *******************************************************************************/
 
-echo HOLA MUNDO
+printf ("HOLA");
 
 ?>
